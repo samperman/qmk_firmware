9fef43fdecbc9037b381a966daba35a03d62399d
Sam Perman <sam@permans.com>
2017-02-15 11:54:08 -0500

    replace pgup and pgdn with volume controls

--- keyboards/first66/keymaps/default/keymap.c
+++ keyboards/first66/keymaps/default/keymap.c
@@ -5,16 +5,17 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BL 0
-#define _FL 1
-#define _RS 2
+#define _ML 1
+#define _FL 2
+#define _RS 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
-   * ,--------------------------------------------------------------------------.  ,----.
-   * |   ~|   1|   2|   3|   4|   5|   6|   7|   8|   9|   0|   -|   =|       BS|  |PGUP|
-   * |--------------------------------------------------------------------------|  |----|
-   * |   Tab|   Q|   W|   E|   R|   T|   Y|   U|   I|   O|   P|   [|   ]|      \|  |PGDN|
-   * |--------------------------------------------------------------------------|  `----'
+   * ,--------------------------------------------------------------------------.  ,-----.
+   * |   ~|   1|   2|   3|   4|   5|   6|   7|   8|   9|   0|   -|   =|       BS|  |_ML,U|
+   * |--------------------------------------------------------------------------|  |-----|
+   * |   Tab|   Q|   W|   E|   R|   T|   Y|   U|   I|   O|   P|   [|   ]|      \|  |_ML,D|
+   * |--------------------------------------------------------------------------|  `-----'
    * |     Esc|   A|   S|   D|   F|   G|   H|   J|   K|   L|   ;|   '|       Ent|
    * |-----------------------------------------------------------------------------.
    * |Shift     |   Z|   X|   C|   V|   B|   N|   M|   ,|   .|   /|      Shift|  UP|
@@ -23,11 +24,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------------------------'
    */
 [_BL] = KEYMAP(
-  KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_PGUP, \
-  KC_TAB,   KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,          KC_PGDN, \
-  KC_ESC,    KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                             \
+  KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,    LT(_ML, KC_VOLU), \
+  KC_TAB,   KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS,    LT(_ML, KC_VOLD), \
+  KC_ESC,    KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,        \
   KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,            \
   KC_LCTL, KC_LGUI, KC_LALT,                                   KC_SPC, KC_RALT,  KC_RCTL,  MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT),
+
+  /* Keymap _ML: Mute Layer
+   * ,--------------------------------------------------------------------------.  ,----.
+   * |    |    |    |    |    |    |    |    |    |    |    |    |    |         |  |MUTE|
+   * |--------------------------------------------------------------------------|  |----|
+   * |      |    |    |    |    |    |    |    |    |    |    |    |    |       |  |MUTE|
+   * |--------------------------------------------------------------------------|  `----'
+   * |        |    |    |    |    |    |    |    |    |    |    |    |          |
+   * |-----------------------------------------------------------------------------.
+   * |          |    |    |    |    |    |    |    |    |    |    |           |    |
+   * |------------------------------------------------------------------------|----|----.
+   * |     |     |     |                             |       |     |  _FL|    |    |    |
+   * `----------------------------------------------------------------------------------'
+   */
+[_ML] = KEYMAP(
+  KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,           KC_MUTE, \
+  KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_MUTE, \
+  KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,                          \
+  KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,            \
+  KC_TRNS, KC_TRNS, KC_TRNS,                           KC_TRNS,KC_TRNS,KC_TRNS,MO(_FL),KC_TRNS,KC_TRNS,KC_TRNS),
 
   /* Keymap _FL: Function Layer
    * ,--------------------------------------------------------------------------.  ,----.
@@ -37,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------------------------------------------------------------------------|  `----'
    * |        |    | _RS|    |    |    |    |    |    |    |    |    |          |
    * |-----------------------------------------------------------------------------.
-   * |          |    |    |    |    |    |    |MUTE|VOLD|VOLU|    |           |PGUP|
+   * |          |    |    |    |    |    |    |    |    |    |    |           |PGUP|
    * |------------------------------------------------------------------------|----|----.
    * |     |     |     |                             |       |     |  _FL|HOME|PGDN| END|
    * `----------------------------------------------------------------------------------'
@@ -46,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,           KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PSCR,KC_SLCK, KC_PAUS,  KC_TRNS,  KC_TRNS,  KC_TRNS,          KC_TRNS, \
   KC_TRNS, KC_TRNS, MO(_RS),KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,                          \
-  KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MUTE,KC_VOLD,KC_VOLU,KC_TRNS,KC_TRNS, KC_PGUP,            \
+  KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_PGUP,            \
   KC_TRNS, KC_TRNS, KC_TRNS,                           KC_TRNS,KC_TRNS,KC_TRNS,MO(_FL),KC_HOME,KC_PGDN,KC_END),
 
   /* Keymap _RS: Reset layer
