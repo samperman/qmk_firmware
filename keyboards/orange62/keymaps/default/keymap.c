001a154742cc7b9cbafb225a9c464100e58f7df6
Sam Perman <sam@permans.com>
2018-01-27 14:25:35 -0500

    works

--- keyboards/orange62/keymaps/default/keymap.c
+++ keyboards/orange62/keymaps/default/keymap.c
@@ -5,10 +5,12 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _BASELAYER 0
-#define _RESET 1
+#define _FUNCTION 1
+#define _SOUND 2
+#define _RESET 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: (Base Layer) Default Layer
+  /* Keymap _BASELAYER: Default Layer
    * ,--------------------------------------------------------------------------.
    * |   ~|   1|   2|   3|   4|   5|   6|   7|   8|   9|   0|   -|   =|       BS|
    * |--------------------------------------------------------------------------|
@@ -22,18 +24,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `--------------------------------------------------------------------------.
    */
 [_BASELAYER] = KEYMAP(
-/* k00,    k01,     k02,    k03,    k04,    k05,    k06,    k07,    k08,    k09,     k0A,      k0B,      k0C,      k0D, \ */
   KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, \
-/* k10,     k11,     k12,    k13,    k14,    k15,    k16,    k17,    k18,    k19,     k1A,      k1B,      k1C,     k1D, \ */
   KC_TAB,   KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS, \
-/* k20,      k21,     k22,    k23,    k24,    k25,    k26,    k27,    k28,    k29,     k2A,      k2B,      k2C, \ */
   KC_ESC,    KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT, \
-/* k30,       k31,     k32,    k33,    k34,    k35,    k36,    k37,    k38,     k39,      k3A,      k3B, \ */
   KC_LSFT,    KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT, \
-/* k40,    k41,     k42,        k43,    k44,  k45,      k46,      k47,     k48 \ */
-  KC_LCTL, KC_LALT, KC_LGUI,    KC_SPC, KC_SPC, KC_RGUI,  KC_RALT,  MO(_RESET), KC_M),
+  KC_LCTL, KC_LALT, KC_LGUI,  KC_SPC, LT(_FUNCTION, KC_SPC), KC_RGUI,  KC_RALT,  MO(_RESET), LT(_SOUND, KC_MUTE) ),
 
-  /* Keymap _RESET: Volume Layer
+  /* Keymap _FUNCTION
+   * ,--------------------------------------------------------------------------.
+   * |    |    |    |    |    |    |    |    |    |    |    |    |    |         |
+   * |--------------------------------------------------------------------------|
+   * |      |    |    |   |     |    |    |    |    |    |    |    |    |       |
+   * |--------------------------------------------------------------------------|
+   * |        |    |    |    |    |    |    |    |    |    |    |    |          |
+   * |--------------------------------------------------------------------------.
+   * |          |    |    |    |    |    |    |    |    |    |    |             |
+   * |--------------------------------------------------------------------------|
+   * ||||     |     |     |           |   FUNC  |       |      |||    |||    ||||
+   * `--------------------------------------------------------------------------'
+   */
+[_FUNCTION] = KEYMAP(
+  KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_UP, KC_TRNS, KC_HOME, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
+
+  /* Keymap _SOUND
+   * ,--------------------------------------------------------------------------.
+   * |    |    |    |    |    |    |    |    |    |    |    |    |    |         |
+   * |--------------------------------------------------------------------------|
+   * |      |    |    |   |     |    |    |    |    |    |    |    |    |       |
+   * |--------------------------------------------------------------------------|
+   * |        |    |    |    |    |    |    |    |    |    |    |    |          |
+   * |--------------------------------------------------------------------------.
+   * |          |    |    |    |    |    |    |    |    |    |    |             |
+   * |--------------------------------------------------------------------------|
+   * ||||     |     |     |           |   FUNC  |       |      |||    |||    ||||
+   * `--------------------------------------------------------------------------'
+   */
+[_SOUND] = KEYMAP(
+  KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_U, KC_VOLU, KC_TRNS, KC_P, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_J, KC_VOLD, KC_L, KC_SCLN, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_M, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LCTL(KC_F2), KC_TRNS ),
+
+  /* Keymap _RESET
    * ,--------------------------------------------------------------------------.
    * |    |    |    |    |    |    |    |    |    |    |    |    |    |         |
    * |--------------------------------------------------------------------------|
