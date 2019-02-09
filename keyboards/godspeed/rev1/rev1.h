959e6c91ad7d9f94b33380c72f622d0b579ce3f7
Sam Perman <sam@permans.com>
2019-02-09 12:19:34 -0500

    right side works

--- keyboards/godspeed/rev1/rev1.h
+++ keyboards/godspeed/rev1/rev1.h
@@ -18,7 +18,7 @@
 //void promicro_bootloader_jmp(bool program);
 
 #define LAYOUT( \
-    L00, L01, L02, L03, L04, L05, L06,       R00, R01, R02, R03, R04, R05, R06, \
+    L00, L01, L02, L03, L04, L05, L06,       R00, R01, R02, R03, R04, R05,      R06, \
     L10, L11, L12, L13, L14, L15,            R10, R11, R12, R13, R14, R15, R16, R17, \
     L20, L21, L22, L23, L24, L25,            R20, R21, R22, R23, R24, R25, R26, \
     L30, L31, L32, L33, L34,      L35,            R30, R31, R32, R33, R34, R35,  \
@@ -30,11 +30,11 @@
         { L20, L21, L22, L23, L24, L25, KC_NO }, \
         { L30, L31, L32, L33, L34, KC_NO, L35 }, \
         { L40, L41, L42, L43, KC_NO, KC_NO, KC_NO }, \
-        { R06, R05, R04, R03, R02, R01, R00, KC_NO }, \
-        { R17, R16, R15, R14, R13, R12, R11, R10 }, \
-        { R26, R25, R24, R23, R22, R21, R20, KC_NO }, \
-        { R35, R34, R33, R32, R31, R30, KC_NO, KC_NO }, \
-        { R43, R42, R41, R40, KC_NO, KC_NO, KC_NO, KC_NO } \
+        { R00, R01, R02, R03, R04, R05, KC_NO, R06 }, \
+        { R10, R11, R12, R13, R14, R15, R16, R17 }, \
+        { R20, R21, R22, R23, R24, R25, R26, KC_NO }, \
+        { KC_NO, R30, R31, R32, R33, R34, R35, KC_NO }, \
+        { KC_NO, R40, R41, KC_NO, R42, R43, KC_NO, KC_NO } \
     }
 
 #endif
