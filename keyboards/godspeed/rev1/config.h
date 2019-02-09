17482efecfa9adffab0c970cce497dc170b59195
Sam Perman <sam@permans.com>
2019-02-09 11:34:43 -0500

    left side works

--- keyboards/godspeed/rev1/config.h
+++ keyboards/godspeed/rev1/config.h
@@ -31,8 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 8
 
 // wiring of each half
-#define MATRIX_ROW_PINS { C6, D7, E6, B4 }
-#define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6, B7 }
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
